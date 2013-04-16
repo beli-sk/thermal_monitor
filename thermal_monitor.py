573428be157eb590c9ae438ec81135550240d845
Beli <devel@beli.sk>
2013-04-16 12:39:33 +0200

    implemented main program as a class

--- thermal_monitor.py
+++ thermal_monitor.py
@@ -28,88 +28,105 @@ import time
 import subprocess
 from optparse import OptionParser
 
-def zabbix_sender(options, data):
-    proc = subprocess.Popen(['zabbix_sender', '-z', options.zabbix, '-p', options.port
-            , '-s', options.host, '-i', '-'], stdin=subprocess.PIPE)
-    for addr,temp in data.items():
-        proc.communicate('- %s[%s] %g\n' % (options.key, addr, temp))
-    proc.stdin.close()
-    proc.wait()
+class ThermalMonitor(object):
 
-parser = OptionParser()
-parser.add_option("-d", "--device", dest="device",
-        help="read from serial port DEVICE (required)", metavar="DEVICE")
-parser.add_option("-s", "--speed", dest="speed", type="int", default=9600,
-        help="serial port baud rate (default: 9600)", metavar="BAUD")
-parser.add_option("-i", "--interval", dest="interval", type="int", default=10,
-        help="sampling interval (default: 10)", metavar="SECONDS")
-parser.add_option("-z", "--zabbix", dest="zabbix",
-        help="Zabbix server (required)", metavar="ADDR")
-parser.add_option("-p", "--port", dest="port", default="10051",
-        help="listening port of Zabbix server (default: 10051)", metavar="PORT")
-parser.add_option("-n", "--host", dest="host",
-        help="name of host in Zabbix (required)", metavar="NAME")
-parser.add_option("-k", "--key", dest="key", default="thermal_monitor",
-        help="item key base name; device address will be added as an argument, "
-        +"e.g. thermal_monitor[addr] (default: thermal_monitor)", metavar="key")
-(options, args) = parser.parse_args()
+    def zabbix_sender(self):
+        proc = subprocess.Popen(['zabbix_sender', '-z', self.options.zabbix, '-p',
+                self.options.port, '-s', self.options.host, '-i', '-'], stdin=subprocess.PIPE)
+        for addr,temp in self.data.items():
+            proc.communicate('- %s[%s] %g\n' % (self.options.key, addr, temp))
+        proc.stdin.close()
+        proc.wait()
 
-# check for required options
-for opt in ['device', 'zabbix', 'host']:
-    if opt not in options.__dict__ or options.__dict__[opt] is None:
-        parser.error("parameter --%s is required" % opt)
+    def parse_options(self):
+        parser = OptionParser()
+        parser.add_option("-d", "--device", dest="device",
+                help="read from serial port DEVICE (required)", metavar="DEVICE")
+        parser.add_option("-s", "--speed", dest="speed", type="int", default=9600,
+                help="serial port baud rate (default: 9600)", metavar="BAUD")
+        parser.add_option("-i", "--interval", dest="interval", type="int", default=10,
+                help="sampling interval (default: 10)", metavar="SECONDS")
+        parser.add_option("-z", "--zabbix", dest="zabbix",
+                help="Zabbix server (required)", metavar="ADDR")
+        parser.add_option("-p", "--port", dest="port", default="10051",
+                help="listening port of Zabbix server (default: 10051)", metavar="PORT")
+        parser.add_option("-n", "--host", dest="host",
+                help="name of host in Zabbix (required)", metavar="NAME")
+        parser.add_option("-k", "--key", dest="key", default="thermal_monitor",
+                help="item key base name; device address will be added as an argument, "
+                +"e.g. thermal_monitor[addr] (default: thermal_monitor)", metavar="key")
+        (self.options, self.args) = parser.parse_args()
+        
+        # check for required options
+        for opt in ['device', 'zabbix', 'host']:
+            if opt not in self.options.__dict__ or self.options.__dict__[opt] is None:
+                parser.error("parameter --%s is required" % opt)
 
-running = True
+    def sighandler_terminate(self, signum, frame):
+        self.running = False
 
-def sighandler_exit(signum, frame):
-    global running
-    running = False
+    def register_signals(self, ignore=[],
+            terminate=[signal.SIGINT, signal.SIGTERM, signal.SIGHUP]):
+        for sig in ignore:
+            signal.signal(sig, signal.SIG_IGN)
+        for sig in terminate:
+            signal.signal(sig, self.sighandler_terminate)
 
-signal.signal(signal.SIGINT, sighandler_exit)
-signal.signal(signal.SIGTERM, sighandler_exit)
-signal.signal(signal.SIGHUP, sighandler_exit)
+    def open_serial(self):
+        self.ser = serial.Serial(self.options.device, self.options.speed)
+        self.ser.readline() # ignore first (incomplete) line
 
-ser = serial.Serial(options.device, options.speed)
-ser.readline() # ignore first (incomplete) line
+    def __init__(self):
+        self.running = False
+        self.data = dict()
 
-cre = re.compile(r"R=(?P<addr>\w+)\s+T=(?P<temp>[.0-9]+)\r?$")
+        self.register_signals()
+        self.parse_options()
+        self.open_serial()
+        self.cre = re.compile(r"R=(?P<addr>\w+)\s+T=(?P<temp>[.0-9]+)\r?$")
 
-next = time.time()
-
-data = dict()
-sent = False
-
-while running:
-    try:
-        line = ser.readline()
-    except select.error as e:
-        if e[0] == 4: # interrupted system call
-            continue
-        else:
-            raise
-    if time.time() > next:
-        next += options.interval
-        # clears the list to send all addresses again
-        for k,v in data.items():
-            data[k] = None
+    def start(self):
+        self.running = True
+        next = time.time()
         sent = False
-    elif sent:
-        # data already sent in this cycle
-        continue
-    m = cre.search(line)
-    if m:
-        # line matched pattern
-        addr = m.group('addr')
-        temp = float(m.group('temp'))
-        if addr not in data or data[addr] is None:
-            # address not yet collected in this cycle
-            data[addr] = temp
-        else:
-            # repeating address reached - send out data
-            print "sending", addr, temp
-            zabbix_sender(options, data)
-            sent = True
-    else:
-        print "invalid line received"
+        while self.running:
+            try:
+                line = self.ser.readline()
+            except select.error as e:
+                if e[0] == 4: # interrupted system call
+                    continue
+                else:
+                    raise
+            if time.time() > next:
+                next += self.options.interval
+                # clears the list to send all addresses again
+                for k,v in self.data.items():
+                    self.data[k] = None
+                sent = False
+            elif sent:
+                # data already sent in this cycle
+                continue
+            m = self.cre.search(line)
+            if m:
+                # line matched pattern
+                addr = m.group('addr')
+                temp = float(m.group('temp'))
+                if addr not in self.data or self.data[addr] is None:
+                    # address not yet collected in this cycle
+                    self.data[addr] = temp
+                else:
+                    # repeating address reached - send out data
+                    print "sending", addr, temp
+                    self.zabbix_sender()
+                    sent = True
+            else:
+                print "invalid line received"
+        self.cleanup()
 
-ser.close()
+    def cleanup(self):
+        self.ser.close()
+
+if __name__ == "__main__":
+    thermalmonitor = ThermalMonitor()
+    thermalmonitor.start()
+
